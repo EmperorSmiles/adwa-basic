bfed393a60e1e5261e3543e9132df467a832c503
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-06 21:37:41 +0100

    feat: pagination controls

---
+++ src/components/UserPagination.tsx
@@ -0,0 +1,58 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationLink,
+} from "./ui/pagination";
+
+interface PaginationControlsProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const UserPagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationControlsProps) => {
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <Pagination>
+      <PaginationContent className="flex items-center justify-center space-x-2">
+        <PaginationItem>
+          <PaginationPrevious
+            onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+            className="bg-gray-800 text-gray-200 hover:bg-gray-700 hover:text-white hover:cursor-pointer"
+          >
+            Previous
+          </PaginationPrevious>
+        </PaginationItem>
+        {pageNumbers.map((page) => (
+          <PaginationItem key={page}>
+            <PaginationLink
+              isActive={currentPage === page}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationNext
+            onClick={() =>
+              currentPage < totalPages && onPageChange(currentPage + 1)
+            }
+            className="bg-gray-800 text-gray-200 hover:bg-gray-700 hover:text-white hover:cursor-pointer"
+          >
+            Next
+          </PaginationNext>
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+export default UserPagination;
